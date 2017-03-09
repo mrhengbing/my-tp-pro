a3085cd411f9f104c919b3b229605b5db4dde974
taihet <mrhengbing@126.com>
2017-03-09 18:21:06 +0800

    3.9 update

--- App/Admin/Controller/InfoflagController.class.php
+++ App/Admin/Controller/InfoflagController.class.php
@@ -3,7 +3,7 @@
  * @Author: mrhengbing
  * @Create time:   2017-02-15 21:12:13
  * @Last Modified by:   mrhengbing
- * @Last Modified time: 2017-03-09 18:11:22
+ * @Last Modified time: 2017-03-09 18:20:01
  * @Email:  415671062@qq.com
  * @----------文章属性模块控制器-------------
  */
@@ -14,7 +14,7 @@ class InfoflagController extends CommonController{
      * 权限验证
      */
     function _initialize(){
-        $this->isModelAuth('infoclass');
+        $this->isModelAuth('infoflag');
     }
     /**
      * 信息标记页
