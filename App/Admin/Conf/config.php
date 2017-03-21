1ba03d41e1e199c262555703153023dc99d3fe9c
hengbing <mrhengbing@126.com>
2017-03-21 16:26:10 +0800

    3.21 update

--- App/Admin/Conf/config.php
+++ App/Admin/Conf/config.php
@@ -4,5 +4,5 @@ return array(
             '__PUBLIC__' => __ROOT__.'/'.APP_NAME.'/'.MODULE_NAME.'/Public', // 设置后台css,js.image目录
     ),
    // 'URL_HTML_SUFFIX' => '', //去掉伪静态后缀   
-
+	'VAR_SESSION_ID'      =>  'session_id',     //sessionID的提交变量
 );
